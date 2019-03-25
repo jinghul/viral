505a80049235bd613fde24de3f6eead38bdf549a
jinghu <jinghul@bu.edu>
2019-03-25 17:02:51 +0800

    p

--- predictor.py
+++ predictor.py
@@ -117,7 +117,7 @@ def main(record):
     social_feature = load_social_features(data_dir + 'video_id.txt', data_dir + 'video_user.txt', data_dir + 'user_details.txt')
 
     # concatenate all the features(after dimension reduction)
-    concat_feature = social_feature
+    concat_feature = visual_feature
     # concat_feature = np.concatenate([text_feature, visual_feature], axis=1)
 
     # remove the empty social feature indices
